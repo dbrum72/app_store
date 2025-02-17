16606e022aff90bb967c114d33a40ab94b3b85f0
dbrum72 <dbrum72.dev@gmail.com>
2025-02-17 16:51:22 -0300

    refatoração do código

---
+++ frontend/src/views/admin/client/getClient.vue
@@ -0,0 +1,172 @@
+<template>
+    <div :class="{ 'blurred': loader.active }">
+        <div class="areaHeader">
+            <span class="font12rW600TuCg">CLIENTE</span>
+        </div>
+
+        <div class="p-2">
+            <div class="subArea mb-5">
+                <div class="d-flex justify-content-between m-2 title-sub-area">
+                    <div>
+                        <span>Dados Pessoais</span>
+                    </div>
+                    <div>
+                        <router-link class="btn btn-sm btn-view" :to="{ name: 'getClients' }"><i
+                                class="fa-solid fa-list"></i></router-link>
+                    </div>
+                </div>
+                <div class="dados">
+                    <div class="tupla">
+                        <div class="field">
+                            ID
+                        </div>
+                        <div class="data">
+                            {{ this.client.id }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Nome
+                        </div>
+                        <div class="data">
+                            {{ client.name }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            CPF/CNPJ
+                        </div>
+                        <div class="data">
+                            {{ client.cnpj_cpf }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Celular
+                        </div>
+                        <div class="data">
+                            {{ client.celphone }}
+                            <i v-if="client.whatsapp" class="fa-brands fa-whatsapp" style="color: #41B883;"></i>
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Email
+                        </div>
+                        <div class="data">
+                            {{ client.email }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="subArea">
+                <div class="title-sub-area">
+                    <span>Endereços</span>
+                    <div>
+                        <router-link class="btn btn-sm btn-view"
+                            :to="{ name: 'newAddress', params: { 'client_id': id } }"><i class="fa-solid fa-plus"></i>
+                            Adicionar endereço</router-link>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column m-2" v-if="(client.addresses && client.addresses.length > 0)">
+                <div class="dados mb-3" v-for="(address, index) in client.addresses" :key="index">
+                    <div class="tupla">
+                        <div class="field">
+                            Logradouro
+                        </div>
+                        <div class="data">
+                            {{ address.street ?? '' }}{{ address.complement ? ', ' + address.complement : '' }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Bairro
+                        </div>
+                        <div class="data">
+                            {{ address.district ?? '' }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Cidade
+                        </div>
+                        <div class="data">
+                            {{ address.city ?? '' }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            CEP
+                        </div>
+                        <div class="data">
+                            {{ address.zipcode ?? '' }}
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                                <router-link class="btn btn-sm btn-edit"
+                                    :to="{ name: 'saveClient', params: { 'id': client.id } }"><i
+                                        class="fa-solid fa-pencil"></i></router-link>
+                                <router-link class="btn btn-sm btn-delete"
+                                    :to="{ name: 'deleteClient', params: { 'id': client.id } }"><i
+                                        class="fa-regular fa-trash-can"></i></router-link>
+                            </div>
+                        </div>
+            </div>
+            <div v-else>
+                <span class="lenght0">Nenhum endereço cadastrado.</span>
+            </div>
+            <div class="subArea">
+                <div class="title-sub-area">
+                    <span>Atualizações</span>
+                </div>
+                <div class="dados">
+                    <div class="tupla">
+                        <div class="field">
+                            Criado em
+                        </div>
+                        <div class="data">
+                            {{ this.client.created_at ? formatDate(this.client.created_at) : '' }}
+                        </div>
+                    </div>
+                    <div class="tupla">
+                        <div class="field">
+                            Atualizado em
+                        </div>
+                        <div class="data">
+                            {{ this.client.updated_at ? formatDate(this.client.updated_at) : '' }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import AbstractMixin from '@/mixins/AbstractMixin';
+import ClientMixin from '@/mixins/ClientMixin';
+
+export default {
+
+    name: 'getClient',
+
+    mixins: [AbstractMixin, ClientMixin],
+
+    data() {
+        return {
+            id: this.$route.params.id,
+            client: {},
+        };
+    },
+
+    computed: mapState(['errors', 'loader']),
+
+    mounted() {
+        this.fetchClient(this.id)
+    }
+
+}
+
+</script>
---
+++ frontend/src/views/admin/client/newAddress.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="content" :class="{ 'blurred': loader.active }">
+        <div class="areaHeader">
+            <span class="font12rW600TuCg">ENDEREÇO (Cliente)</span>
+        </div>
+
+        <div class="p-2">
+            <div>
+                <div class="row">
+                    <div class="col-sm-12 col-lg-8 mb-2">
+                        <h6>Nova entrada...</h6>
+                    </div>
+                    <div class="col-sm-12 col-lg-4 mb-2">
+                        <div class="form-floating">
+                            <div class="form-check text-start">
+                                <input type="checkbox" class="form-check-input" true-value="1" v-model="address.main">
+                                <label class="form-check-label" for="AddressMain">
+                                    Principal ?
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 mb-2">
+                        <div class="form-floating">
+                            <input type="text" v-model="client.name" placeholder="Cliente"
+                                class="form-control" readonly />
+                            <label for="addressClientId">Cliente</label>
+                            <div v-if="errors.client_id" id="addressClientIdFeedback"
+                                class="invalid-feedback text-start">{{ errors.client_id }}
+                            </div>
+                            <div>
+                                <input type="text" v-model="address.client_id" hidden />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="row">
+                        <div class="col-sm-12 col-lg-8 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" :class="errors.street ? 'is-invalid' : ''"
+                                    id="addressStreet" aria-describedby="addressStreetFeedback" placeholder="Logradouro"
+                                    v-model="address.street">
+                                <label for="addressStreet">Logradouro *</label>
+                                <div v-if="errors.street" id="addressStreetFeedback"
+                                    class="invalid-feedback text-start">{{ errors.street[0] }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-4 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" :class="errors.zipcode ? 'is-invalid' : ''"
+                                    id="addressZipcode" aria-describedby="addressZipcodeFeedback" placeholder="CEP"
+                                    v-model="address.zipcode">
+                                <label for="addressZipcode">CEP</label>
+                                <div class="input-group-append">
+                                    <button class="btn btn-opcoes search-button" type="button"
+                                        @click="searchZipCode(address.zipcode)"><i
+                                            class="fa-solid fa-magnifying-glass"></i></button>
+                                </div>
+                                <div v-if="errors.zipcode" id="addressZipcodeFeedback"
+                                    class="invalid-feedback text-start">{{ errors.zipcode[0] }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" :class="errors.complement ? 'is-invalid' : ''"
+                                    id="addressComplement" aria-describedby="addressComplementFeedback"
+                                    placeholder="Complemento" v-model="address.complement">
+                                <label for="addressComplement">Complemento</label>
+                                <div v-if="errors.complement" id="addressComplementFeedback"
+                                    class="invalid-feedback text-start">{{ errors.complement[0] }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12 col-lg-6 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" :class="errors.district ? 'is-invalid' : ''"
+                                    id="addressDistrict" aria-describedby="addressDistrictFeedback" placeholder="Bairro"
+                                    v-model="address.district">
+                                <label for="addressDistrict">Bairro *</label>
+                                <div v-if="errors.district" id="addressDistrictFeedback"
+                                    class="invalid-feedback text-start">{{ errors.district[0] }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-12 col-lg-6 mb-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" :class="errors.city ? 'is-invalid' : ''"
+                                    id="addressCity" aria-describedby="addressCityFeedback" placeholder="Cidade"
+                                    v-model="address.city">
+                                <label for="addressCity">Cidade *</label>
+                                <div v-if="errors.city" id="addressCityFeedback" class="invalid-feedback text-start">{{
+                                    errors.city[0] }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <span class="obgField">* Campo obrigatório.</span>
+                        </div>
+                        <div class="card-footer text-end">
+                            <button type="button" class="btn btn-cancel me-2"
+                                @click="(template = 'list')">Cancelar</button>
+                            <button class="btn btn-save" type="button"
+                                @click="saveAddress(address.id ?? null)">Salvar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import AbstractMixin from '@/mixins/AbstractMixin';
+import ClientAddressMixin from '@/mixins/ClientAddressMixin';
+import ClientMixin from '@/mixins/ClientMixin';
+
+export default {
+
+    name: 'NewAddress',
+
+    mixins: [AbstractMixin, ClientAddressMixin, ClientMixin],
+
+    data() {
+        return {
+            client: {},
+            client_id: this.$route.params.client_id,
+            address: {},            
+            loading: false
+        };
+    },
+
+    computed: mapState(['loader', 'errors']),
+
+    mounted() {
+        this.fetchClient(this.client_id)
+        this.address.client_id = this.client_id
+    }
+}
+</script>
+
+<style>
+
+</style>
